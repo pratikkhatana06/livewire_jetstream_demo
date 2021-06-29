a4fb6a79088e6429060946b5100a944e00156c2a
NAROLA\kpratik <pbk@narola.email>
2021-06-29 09:54:34 +0530

    First time submit

---
+++ resources/views/livewire/crud.blade.php
@@ -0,0 +1,52 @@
+<x-slot name="header">
+    <h2 class="text-center">Laravel 8 Livewire CRUD Demo</h2>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</x-slot>
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-4">
+            @if (session()->has('message'))
+            <div class="alert alert-success" 
+                role="alert">
+                <div class="flex">
+                    <div>
+                        <p class="text-sm">{{ session('message') }}</p>
+                    </div>
+                </div>
+            </div>
+            @endif
+            <button wire:click="create()"
+                 class="btn btn-success">Create Student</button>
+            @if($isModalOpen)
+            @include('livewire.create')
+            @endif
+            <table class="table-fixed w-full">
+                <thead>
+                    <tr class="bg-gray-100">
+                        <th class="px-4 py-2 w-20">No.</th>
+                        <th class="px-4 py-2">Name</th>
+                        <th class="px-4 py-2">Email</th>
+                        <th class="px-4 py-2">Mobile</th>
+                        <th class="px-4 py-2">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($students as $student)
+                    <tr>
+                        <td class="border px-4 py-2">{{ $student->id }}</td>
+                        <td class="border px-4 py-2">{{ $student->name }}</td>
+                        <td class="border px-4 py-2">{{ $student->email}}</td>
+                        <td class="border px-4 py-2">{{ $student->mobile}}</td>
+                        <td class="border px-4 py-2">
+                            <button wire:click="edit({{ $student->id }})"
+                                class="btn btn-primary">Edit</button>
+                            <button wire:click="delete({{ $student->id }})"
+                                class="btn btn-danger">Delete</button>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
